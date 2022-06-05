57453bdc6b92fc62547cac089207cc992fa81fb4
unknown <mnasri89mnasri@gmail.com>
2022-06-06 01:12:06 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import {movieData } from './Data';
+import React, { useState } from 'react';
+import Filter from './Components/FilterRate/FilterRate.js'
+import MovieList from './Components/MovieList/MovieList.js'
+import AddMovie from './Components/AddMovie/AddMovie.js'
+import './App.css';
+function App() {
+  const [movies,setMovies] = useState(movieData);
+  const [search,setSearch] = useState('');
+  const [rate,setRate] = useState(0);
+  const handleAddMovie = (newMovie) => {
+    setMovies([...movies, newMovie])
+  }
+  
+  const searching = (s) => {
+    setSearch(s)
+  }
+  const rating = (r) => {
+    setRate(r)
+  }
+  return (
+    <div>
+            <h1 className='AppTitle'>Movies</h1>
+      <div className='nav'>
+        <Filter searching={searching} rate={rating}/>
+      </div>
+      <div className='movie-list'> 
+      <AddMovie handleAddMovie={handleAddMovie} />
+      </div>
+      <div>
+      {movieData.length?
+      <MovieList movieData={movies.filter((elm) => (elm.title.toLowerCase().match(search.toLowerCase().trim())) && (elm.rate >= rate))}/>
+      :<h1>No Movies</h1>
+}
+      </div>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,100 @@
+export const movieData = [
+    {
+        
+        title : "Kate " ,
+        description: "After she's irreversibly poisoned, a ruthless criminal operative has less than 24 hours to exact revenge on her enemies and in the process forms an unexpected bond with the daughter of one of her past victims.",
+        posterURL : "https://fr.web.img5.acsta.net/pictures/21/08/16/10/26/0653161.jpg",
+        rate : 3 ,
+        filmTrailer: "https://www.youtube.com/embed/MysGjRS9jFU" ,
+       
+    } ,
+    
+     {
+        
+        title : "What If...?" ,
+        description: "Taking inspiration from the comic books of the same name, each episode explores a pivotal moment from the Marvel Cinematic Universe and turns it on its head, leading the audience into uncharted territory.",
+        posterURL : "https://www.denofgeek.com/wp-content/uploads/2021/07/marvel-what-if-trailer-breakdown-easter-eggs.jpg?fit=1280%2C720",
+        rate : 3.5 ,
+        filmTrailer:"https://www.youtube.com/embed/x9D0uUKJ5KI",
+       
+    } ,
+     {
+      
+        title : "Infinite" ,
+        description: "Evan McCauley has skills he never learned and memories of places he has never visited. Self-medicated and on the brink of a mental breakdown, a secret group that call themselves “Infinites” come to his rescue, revealing that his memories are real.",
+        posterURL : "https://fr.web.img2.acsta.net/pictures/21/06/10/10/03/0580430.jpg",
+        rate : 3.5 ,
+        filmTrailer:"https://www.youtube.com/embed/a3RDnD9YVxA",
+       
+     } ,
+         {
+        
+        title : "Game of Thrones " ,
+        description: "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+        posterURL : "https://i.egycdn.com/pic/WmFwZndlY3ZjbWp2dGNtRW1hY2F3ZGZOam16UG1n.jpg",
+        rate : 4.5 ,
+        filmTrailer:"https://www.youtube.com/embed/rlR4PJn8b8I",
+       
+    } ,
+    {
+       
+        title : "Bartkowiak " ,
+        description: "After his brother dies in a car crash, a disgraced MMA fighter takes over the family nightclub and soon learns his sibling's death wasn’t an accident.",
+        posterURL : "https://fr.web.img2.acsta.net/pictures/21/07/06/20/48/0430456.jpg",
+        rate : 4.5 ,
+        filmTrailer:"https://www.youtube.com/embed/Y4dawT5HGeY",
+       
+    
+    } ,
+    {
+       
+        title : "Heels " ,
+        description: "In a close-knit Georgia community, a family-owned wrestling promotion finds two brothers and rivals war over their late father’s legacy. In the ring, somebody must play the good guy and somebody must play their nemesis, the heel. But in the real world, those characters can be hard to live up to (and just as hard to leave behind).",
+        posterURL : "https://m.media-amazon.com/images/M/MV5BZmNmZjc2YmUtYTQ0OC00Y2FiLWFhMGUtZjQ3YzY3NWRkZDU3XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg",
+        rate : 4 ,
+        filmTrailer:"https://www.youtube.com/embed/dKFfSSXavYc",
+        
+    } ,
+    {
+       
+        title : "Jolt " ,
+        description: "Lindy is an acid-tongued woman with rage issues who controls her temper by shocking herself with an electrode vest. One day she makes a connection with Justin, who gives her a glimmer of hope for a shock-free future, but when he’s murdered she launches herself on a revenge-fueled rampage in pursuit of his killer.",
+        posterURL : "https://image.tmdb.org/t/p/w300/Ldo7P7EjiPjdohwXfG9jdjIGAP.jpg",
+        rate : 3 ,
+        filmTrailer:"https://www.youtube.com/embed/EMzgOCHzPMs",
+       
+       
+     
+            } ,
+     {
+        
+        title : "S.O.Z: Soldiers or Zombies " ,
+        description: "Drug lord Alonso Marroquín escapes from a Mexican prison in an attempt to go into hiding. A US military secret experiment goes wrong and the elite unit from the Mexican Police that is after Marroquín gets infected, creating a new zombie species. The Army and the zombies end up at the drug lord’s hideout and a battle for human survival begins.",
+        posterURL : "https://fr.web.img3.acsta.net/c_310_420/pictures/21/08/02/17/37/3285381.jpg",
+        rate : 4 ,
+        filmTrailer:"https://www.youtube.com/embed/KH8wP8dSQtI",
+        
+       
+    
+    } ,
+     {
+      
+        title : "Nightbooks  " ,
+        description: "Alex, a boy obsessed with scary stories, is trapped by a witch in her modern, magical New York City apartment. His original hair-raising tales are the only thing keeping him safe as he desperately tries to find a way out of this twisted place.",
+        posterURL : "https://m.media-amazon.com/images/M/MV5BY2E4MmNmMzgtOTI2YS00MzhjLTlmOWItZTkxNWRkYTY2NDcwXkEyXkFqcGdeQXVyNjY1MTg4Mzc@._V1_.jpg",
+        rate : 5 ,
+        filmTrailer:"https://www.youtube.com/embed/aQO16k5Vdow",
+       
+       
+    
+    } ,
+     {
+       
+        title : "The Good Doctor   " ,
+        description: "A young surgeon with Savant syndrome is recruited into the surgical unit of a prestigious hospital. The question will arise: can a person who doesn't have the ability to relate to people actually save their lives",
+        posterURL : "https://fr.web.img5.acsta.net/pictures/18/09/12/17/44/5464494.jpg",
+        rate : 5 ,
+        filmTrailer:"https://www.youtube.com/embed/fYlZDTru55g",
+       
+    }
+  ];
